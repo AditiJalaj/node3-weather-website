0484c5fc2ee1bbd8ceff849ab2cd31f84b1f559e
ajalaj <aditi.jalaj@gmail.com>
2021-01-09 22:25:02 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,111 @@
+const path=require('path')
+const express=require('express')
+const hbs=require('hbs')
+const geocode=require('./utils/geocode.js')
+const forecast=require('./utils/forecast.js')
+
+console.log(__dirname)
+console.log(path.join(__dirname,'../public'))
+
+const app=express()
+
+//define paths for express config
+const publicDirectoryPath=path.join(__dirname,'../public')
+const viewsPath=path.join(__dirname,'../templates/views')  //customize the views directory
+const partialsPath=path.join(__dirname,'../templates/partials')
+
+//setuphandlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+//setup static directory to set
+app.use(express.static(publicDirectoryPath))
+
+//set up route to index.hbs
+app.get('',(req,res)=>{
+    res.render('index.hbs',{
+        title:'Weather App',
+        name:'Adeetayyy'
+    })
+})
+
+
+//below we are setting up route to about.hbs
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        title:'About me',
+        name:'Adeetayyy'
+    })
+})
+
+
+//set up route to help.hbs
+app.get('/help',(req,res)=>
+{
+    res.render('help.hbs',{
+        message:'this is a helpful page',
+        title:'Help',
+        name:'Adeetayyy'
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error:'You must enter an address'
+        })
+    }
+    console.log(req.query.address)
+    geocode(req.query.address,(error,{latitude,longitude,location}={})=>
+    {
+        if(error){
+            return res.send({error})
+        }
+        forecast(latitude,longitude,(error,forecastData)=>{
+            if(error){
+                return res.send({error})
+            }
+            res.send({
+                forecast:forecastData,
+                location,
+                address:req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+         //if no search term
+        return res.send({
+             error:'You must provide a search term'
+         })
+    }
+    console.log(req.query.search)
+    res.send({
+        products:[]
+    })
+})
+app.get('/help/*',(req,res)=>
+{
+    res.render('404.hbs', {
+        title:'404',
+        name:'Adeetayyy',
+        message:'Help article not found'})
+})
+
+//set up 404 page
+app.get('*',(req,res)=>{
+    res.render('404.hbs',{
+        title:'404',
+        name:'Adeetayyy',
+        message:'Page not found'})
+})
+
+//start the server ,done only once!
+app.listen(3000,()=>
+{
+    console.log('Server is up on port 3000.')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request=require('request')
+const forecast=(lat,long,callback)=>{
+    const url='http://api.weatherstack.com/current?access_key=43843c1b297b7df21b9c2b26c6a749bb&query='+lat+','+long+'&units=m'
+    request({url,json:true},(error,{body})=>{
+        if(error)
+        {
+            callback('Unable to connect to weather service',undefined)
+        }
+        else if(body.error){
+            callback('Unable to find location',undefined)
+        }
+        else{
+            callback(undefined,
+                (body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degrees out.It feels like "+body.current.feelslike+" degrees out.")
+            )
+        }
+    })
+}
+
+module.exports=forecast
